8f260eb8626b17568fb81717801126dacd07691e
vmshaneeb <vmshaneeb@gmail.com>
2020-08-07 00:53:07 +0530

    Initial Commit

---
+++ routerConfig/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  routerConfig
+//
+//  Created by M Shaneeb on 7/8/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ routerConfig/routerConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  routerConfigApp.swift
+//  routerConfig
+//
+//  Created by M Shaneeb on 7/8/2020.
+//
+
+import SwiftUI
+
+@main
+struct routerConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
